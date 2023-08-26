f1ad79658b211b956df37a0372c0fabfeb72c856
alisterd51 <antoinereims28@gmail.com>
2023-08-26 02:06:55 +0200

    fix ft_strcpy

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -3,17 +3,19 @@
     .globl ft_strcpy
 
 ft_strcpy:
-    mov rcx, 0
+    push    rcx
+    mov     rcx, 0
 .LBB_loop:
-    mov al, [rsi + rcx]
-    cmp al, 0
-    je  .LBB_end
-    mov [rdi + rcx], al
-    inc rcx
-    jmp .LBB_loop
+    mov     al, [rsi + rcx]
+    cmp     al, 0
+    je      .LBB_end
+    mov     [rdi + rcx], al
+    inc     rcx
+    jmp     .LBB_loop
 .LBB_end:
-    mov byte ptr [rdi + rcx], 0
-    mov rax, rdi
+    mov     byte ptr [rdi + rcx], 0
+    mov     rax, rdi
+    pop     rcx
     ret
 .end:
     .section    ".note.GNU-stack","",@progbits
